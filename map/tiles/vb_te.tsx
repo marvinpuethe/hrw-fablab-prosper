d8d656ea65c50a093942a51b9b9c5624146bcc3a
TheJek <81150097+TheJek@users.noreply.github.com>
2021-04-20 13:10:46 +0200

    Add files via upload

---
+++ map/tiles/vb_te.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="teams" tilewidth="32" tileheight="32" tilecount="120" columns="10">
+ <image source="../tiles/teams.png" width="320" height="384"/>
+</tileset>
